065a700bd3d079f90bb286fc4b4255e19940383b
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-01-18 05:56:33 -0500

    optimize isomorphicStrings

--- isomorphicStrings.js
+++ isomorphicStrings.js
@@ -39,35 +39,33 @@
 // s and t consist of any valid ascii character.
 
 // Solution
-// iterate over s and t simultaneously, using index i, and initializing an empty map
-// for every character of s and t, check to see if the character of s[i] is in the map
-// if it is not in the map, set it as a key in the map and its value to t[i]
-// if it is in the map, and the value is not already t[i], then return false because that letter would have already been mapped to a different letter
-// if it's in the map and its value is t[i] just ignore it, it's ok
+// initialize an empty map
+// iterate over s and t simultaneously, keeping track of sChar and tChar each iteration
+// for every iteration, check to see if the character of s[i] is not in the map
+// if sChar is not a key in the map, check to see if the tChar IS in the map Object's values; if so, return false because that means tChar was assigned as value to another key
+// if tChar is not a value in the map, set map[sChar] to the value of tChar
+// if sChar is already in the map as a key, then check to make sure that its value is tChar; if it is not, return false because that means sChar was assigned to a different value already
 // if we haven't returned false when we finish the loop, return true
+
+// Time Complexity - O(n) because we have to iterate over every character of s.  Iterating over the Object.values(map) on line 60 should be constant time
+// because the number of unique keys and values in that map is bound to the number of ascii characters.
+// Space Complexity - O(1) because again, the number of unique keys we can have in map is bound to the number of ascii characters.
 
 const isIsomorphic = function (s, t) {
   const map = {};
   for (let i = 0; i < s.length; i++) {
     const sChar = s[i];
     const tChar = t[i];
-    const keys = Object.keys(map);
-    const values = Object.values(map);
-    if (keys.includes(sChar) && !values.includes(tChar)) {
+    if (!map[sChar]) {
+      if (Object.values(map).includes(tChar)) {
+        return false;
+      }
+      map[sChar] = tChar;
+    } else if (map[sChar] !== tChar) {
       return false;
     }
-    if (values.includes(tChar) && !keys.includes(sChar)) {
-      return false;
-    }
-    if (
-      keys.includes(sChar) &&
-      values.includes(tChar) &&
-      map[sChar] !== tChar
-    ) {
-      return false;
-    }
-    map[sChar] = tChar;
   }
+
   return true;
 };
 
